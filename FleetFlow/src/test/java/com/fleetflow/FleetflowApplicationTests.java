1f7fa835292d5970ccb819787ed73969334c24d4
hatim <hatimacharaoui@gmail.com>
2026-06-08 10:51:09 +0100

    fix workflow and tests

--- FleetFlow/src/test/java/com/fleetflow/FleetflowApplicationTests.java
+++ FleetFlow/src/test/java/com/fleetflow/FleetflowApplicationTests.java
@@ -2,11 +2,16 @@ package com.fleetflow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class FleetflowApplicationTests {
+
+	@MockBean
+	private UserDetailsService userDetailsService;
 
 	@Test
 	void contextLoads() {
